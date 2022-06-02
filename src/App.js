2310c8d027d6ac365e9da2b4d8ecc45cae2b0bc9
iaasifkhan <thousifg1999@gmail.com>
2022-06-02 09:34:11 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Classroom from "./classrooom/classroom";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Items from "./items/items";
 import Data from "./data/data";
 
 function App() {
